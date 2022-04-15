dffe493de7303b8ef79570e3d585e4f049bc16a5
kborisov <kborisov@lely.com>
2022-04-15 13:30:37 +0300

    Income/Expense details page added

--- squandered/transaction/urls.py
+++ squandered/transaction/urls.py
@@ -7,4 +7,6 @@ urlpatterns = (
     path('add-expense-category/', views.AddExpenseCategoryView.as_view(), name='add expense category'),
     path('add-income/', views.AddIncomeView.as_view(), name='add income'),
     path('add-income-category/', views.AddIncomeCategoryView.as_view(), name='add income category'),
+    path('expenses-details/', views.ExpensesDetailsView.as_view(), name='expenses details'),
+    path('income-details/', views.IncomeDetailsView.as_view(), name='income details')
 )
--- squandered/transaction/views.py
+++ squandered/transaction/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views import generic as views
 
 from squandered.transaction.forms import AddExpenseForm, AddExpenseCategoryForm, AddIncomeForm, AddIncomeCategoryForm
+from squandered.transaction.models import Expense, Income
 
 
 class BaseTransactionView(LoginRequiredMixin, views.CreateView):
@@ -34,3 +35,15 @@ class AddIncomeCategoryView(BaseTransactionView):
     form_class = AddIncomeCategoryForm
     template_name = 'transaction/add-income-category.html'
     success_url = reverse_lazy('add income')
+
+
+class ExpensesDetailsView(LoginRequiredMixin, views.ListView):
+    template_name = 'transaction/expenses-details.html'
+    model = Expense
+    context_object_name = 'expenses'
+
+
+class IncomeDetailsView(LoginRequiredMixin, views.ListView):
+    template_name = 'transaction/income-details.html'
+    model = Income
+    context_object_name = 'incomes'
